d02e09ab061f6000df81aa2aa5733cabba480266
Vejen-control <111349061+Vejen-control@users.noreply.github.com>
2026-09-11 20:27:44 +0100

    Add files via upload

--- js/contact.js
+++ js/contact.js
@@ -25,7 +25,7 @@ const response = await fetch(
     {
         method: "POST",
         headers: {
-            "Content-Type": "application/json"
+            "Content-Type": "text/plain;charset=utf-8"
         },
         body: JSON.stringify(formData)
     }
