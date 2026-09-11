1bac23dcbba2f5b6a473d618ba2c097f668d967c
Vejen-control <111349061+Vejen-control@users.noreply.github.com>
2026-09-11 20:05:26 +0100

    Add files via upload

---
+++ js/contact.js
@@ -0,0 +1,52 @@
+const form=document.getElementById("contactForm");
+
+if(form){
+
+form.addEventListener("submit",async(e)=>{
+
+e.preventDefault();
+
+
+try{
+document.getElementById("status").innerHTML=
+
+"Sending...";
+
+const formData = {
+    name: form.name.value,
+    company: form.company.value,
+    email: form.email.value,
+    subject: form.subject.value,
+    message: form.message.value
+};
+
+const response = await fetch(
+    "https://script.google.com/macros/s/AKfycby6qi0zfvIYA1zvXZLGIch6v3-Dkxcuxar7WbTlBhrMqkVYT5afdg7HmKdQXHexRyaOab/exec",
+    {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(formData)
+    }
+);
+
+const result=await response.text();
+
+document.getElementById("status").innerHTML=result;
+
+form.reset();
+
+}
+
+catch{
+
+document.getElementById("status").innerHTML=
+
+"Unable to send enquiry.";
+
+}
+
+});
+
+}
